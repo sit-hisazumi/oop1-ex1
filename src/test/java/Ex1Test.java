a6ef369615522c36343f991e166d11dae623defe
Kenji Hisazumi <hisazumi@gmail.com>
2024-05-27 08:25:43 +0900

    Refactor: Replace array usage with List

--- src/test/java/Ex1Test.java
+++ src/test/java/Ex1Test.java
@@ -3,6 +3,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class Ex1Test {
     @Test
     public void testAdd() {
@@ -13,40 +15,17 @@ public class Ex1Test {
 
     @Test
     public void testAssign1toN() {
-        int[] array;
-        
-        array = new int[0];
-        Ex1.assign1toN(array, 0);
-        assertThat(array).containsExactly();
-
-        array = new int[1];
-        Ex1.assign1toN(array, 1);
-        assertThat(array).containsExactly(1);
-
-        array = new int[10];
-        Ex1.assign1toN(array, 10);
-        assertThat(array).containsExactly(1,2,3,4,5,6,7,8,9,10);
+        assertThat(Ex1.assign1toN(0)).containsExactly();
+        assertThat(Ex1.assign1toN(1)).containsExactly(1);
+        assertThat(Ex1.assign1toN(10)).containsExactly(1,2,3,4,5,6,7,8,9,10);
     }
 
     @Test
     public void testAssign1toNofEven() {
-        int[] array;
-
-        array = new int[0];
-        Ex1.assign1toNofEven(array, 0);
-        assertThat(array).containsExactly();
-
-        array = new int[1];
-        Ex1.assign1toNofEven(array, 2);
-        assertThat(array).containsExactly(2);
-
-        array = new int[2];
-        Ex1.assign1toNofEven(array, 4);
-        assertThat(array).containsExactly(2, 4);
-
-        array = new int[5];
-        Ex1.assign1toNofEven(array, 10);
-        assertThat(array).containsExactly(2, 4, 6, 8, 10);
+        assertThat(Ex1.assign1toNofEven(0)).containsExactly();
+        assertThat(Ex1.assign1toNofEven(2)).containsExactly(2);
+        assertThat(Ex1.assign1toNofEven(4)).containsExactly(2, 4);
+        assertThat(Ex1.assign1toNofEven(10)).containsExactly(2, 4, 6, 8, 10);
     }
 
     @Test
@@ -81,23 +60,20 @@ public class Ex1Test {
 
     @Test
     public void testTotal() {
-        assertEquals(0, Ex1.total(new int[]{}));        
-        assertEquals(3, Ex1.total(new int[]{1,2}));        
-        assertEquals(55, Ex1.total(new int[]{1,2,3,4,5,6,7,8,9,10}));        
+        assertEquals(0, Ex1.total(List.of()));
+        assertEquals(3, Ex1.total(List.of(1, 2)));
+        assertEquals(55, Ex1.total(List.of(1,2,3,4,5,6,7,8,9,10)));
     }
 
     @Test
     public void testAverage() {
-        assertEquals(1.5, Ex1.average(new int[]{1,2}), 0.0001);        
-        assertEquals(5.5, Ex1.average(new int[]{1,2,3,4,5,6,7,8,9,10}), 0.0001);        
+        assertEquals(1.5, Ex1.average(List.of(1,2)), 0.0001);
+        assertEquals(5.5, Ex1.average(List.of(1,2,3,4,5,6,7,8,9,10)), 0.0001);
     }
 
     @Test
     public void testTable9x9() {
-        int[] array = new int[9*9];
-        Ex1.table9x9(array);
-
-        assertThat(array)
+        assertThat(Ex1.table9x9())
             .containsExactly(1,2,3,4,5,6,7,8,9,
                             2,4,6,8,10,12,14,16,18,
                             3,6,9,12,15,18,21,24,27,
@@ -137,11 +113,7 @@ public class Ex1Test {
 
     @Test
     public void testPerpetualCalendar() {
-        int[] array;
-        
-        array = new int[37];
-        Ex1.perpetualCalendar(array, 2020, 8);
-        assertThat(array)
+        assertThat(Ex1.perpetualCalendar(2020, 8))
             .containsExactly(0, 0, 0, 0, 0, 0, 1, 
                             2, 3, 4, 5, 6, 7, 8, 
                             9, 10, 11, 12, 13, 14, 15, 
@@ -149,15 +121,11 @@ public class Ex1Test {
                             23, 24, 25, 26, 27, 28, 29, 
                             30, 31);
 
-        array = new int[37];
-        Ex1.perpetualCalendar(array, 2020, 2);
-        assertThat(array)
+        assertThat(Ex1.perpetualCalendar(2020, 2))
             .containsExactly(0, 0, 0, 0, 0, 0, 1, 
                             2, 3, 4, 5, 6, 7, 8, 
                             9, 10, 11, 12, 13, 14, 15, 
                             16, 17, 18, 19, 20, 21, 22, 
-                            23, 24, 25, 26, 27, 28, 29, 
-                            0, 0);
-
+                            23, 24, 25, 26, 27, 28, 29);
     }
 }
